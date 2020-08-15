78ce66c3a489091674f63d9ebab63625317fe7a2
dessarion <dessuas@gmial.com>
2020-08-15 13:34:31 +0300

    todo app

---
+++ src/components/TodoList.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <ul>            
+            <Todoitem
+            v-for="(todo, i) in todos"
+            :todo="todo"
+            :index="i"
+            :key="todo.id"
+            v-on:remove-todo="removeTodo" 
+            />
+        </ul>
+    </div>
+</template>
+
+<script>
+import Todoitem from '@/components/Todoitem'
+export default {
+    name: 'TodoList',
+    props: ['todos'],
+    components: {
+        Todoitem,
+    },
+    methods: {
+        removeTodo(id){
+            this.$emit('remove-todo', id)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    ul{
+        list-style: none;
+        margin: 0;
+        padding: 0;
+    }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h2>Home page</h2>
+        <p>This is home page from "views" folder to test multi page interaction</p>
+        <router-link to="/todos">Todos</router-link>
+    </div>
+</template>
